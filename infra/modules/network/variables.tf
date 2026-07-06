496c6de3dfad3397fc632ac464b3c79616b1b9ea
varsha18-droid <varsha.sharma180499@gmail.com>
2026-07-06 10:49:30 +0530

    Update variables.tf

--- infra/modules/network/variables.tf
+++ infra/modules/network/variables.tf
@@ -1,23 +1,4 @@
-variable "vpc_id" {
-  type = string
-}
-
-variable "private_subnet_1_id" {
-  type = string
-}
-
-variable "private_subnet_2_id" {
-  type = string
-}
-
-variable "ecs_security_group_id" {
-  type = string
-}
-
-variable "target_group_arn" {
-  type = string
-}
-
-variable "aws_region" {
-  type = string
-}
+variable "cidr" {}
+variable "name" {}
+variable "public_subnet_cidr" {}
+variable "az" {}
