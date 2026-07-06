b0971f6839a74981a3800ebf56a0d01fe3f19cf7
varsha18-droid <varsha.sharma180499@gmail.com>
2026-07-06 10:49:04 +0530

    Update main.tf

--- infra/modules/network/main.tf
+++ infra/modules/network/main.tf
@@ -1,9 +1,12 @@
-resource "aws_vpc" "main" {
-  cidr_block           = var.vpc_cidr
-  enable_dns_support   = true
-  enable_dns_hostnames = true
-
+resource "aws_vpc" "this" {
+  cidr_block = var.cidr
   tags = {
-    Name = "devops-assessment-vpc"
+    Name = var.name
   }
+}
+
+resource "aws_subnet" "public" {
+  vpc_id            = aws_vpc.this.id
+  cidr_block        = var.public_subnet_cidr
+  availability_zone = var.az
 }
