f67ce32a9b7b328214ccae12d14983d5592dbf70
varsha18-droid <varsha.sharma180499@gmail.com>
2026-07-06 10:50:23 +0530

    Update main.tf

--- infra/modules/ecs/main.tf
+++ infra/modules/ecs/main.tf
@@ -1,5 +1,23 @@
-# ECS Module
+resource "aws_ecs_cluster" "this" {
+  name = var.cluster_name
+}
 
-resource "aws_ecs_cluster" "cluster" {
-  name = "assessment-cluster"
+resource "aws_ecs_task_definition" "this" {
+  family                   = var.task_family
+  requires_compatibilities = ["FARGATE"]
+  network_mode             = "awsvpc"
+
+  cpu    = var.cpu
+  memory = var.memory
+
+  container_definitions = jsonencode([
+    {
+      name  = "app"
+      image = var.image
+      essential = true
+      portMappings = [{
+        containerPort = 80
+      }]
+    }
+  ])
 }
