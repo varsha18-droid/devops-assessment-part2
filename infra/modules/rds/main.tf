fc28290f833cb3fa73633f8285e2a6c7c67b7bd7
varsha18-droid <varsha.sharma180499@gmail.com>
2026-07-06 10:56:40 +0530

    Update main.tf

--- infra/modules/rds/main.tf
+++ infra/modules/rds/main.tf
@@ -1,1 +1,14 @@
+resource "aws_db_instance" "this" {
+  identifier        = var.db_name
+  engine            = "mysql"
+  instance_class    = var.instance_class
+  allocated_storage = var.storage
 
+  db_name  = "appdb"
+  username = "admin"
+  password = var.password
+
+  skip_final_snapshot     = var.skip_final_snapshot
+  backup_retention_period = var.backup_retention
+  deletion_protection     = var.deletion_protection
+}
