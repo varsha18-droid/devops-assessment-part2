1b9dd81173e306517c0f4388a53a3c492f2a92c0
varsha18-droid <varsha.sharma180499@gmail.com>
2026-07-06 10:49:54 +0530

    Update outputs.tf

--- infra/modules/network/outputs.tf
+++ infra/modules/network/outputs.tf
@@ -1,23 +1,7 @@
 output "vpc_id" {
-  value = aws_vpc.main.id
+  value = aws_vpc.this.id
 }
 
-output "public_subnet_1_id" {
-  value = aws_subnet.public_1.id
-}
-
-output "public_subnet_2_id" {
-  value = aws_subnet.public_2.id
-}
-
-output "private_subnet_1_id" {
-  value = aws_subnet.private_1.id
-}
-
-output "private_subnet_2_id" {
-  value = aws_subnet.private_2.id
-}
-
-output "db_subnet_group_name" {
-  value = aws_db_subnet_group.db_subnet_group.name
+output "subnet_id" {
+  value = aws_subnet.public.id
 }
